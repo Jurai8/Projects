b10e9776502176ec58003e07cb4d357619cdbb3f
Charles <131194505+Jurai8@users.noreply.github.com>
2024-06-24 15:54:17 +0000

    delete

--- polyglot2/polyglot/src/App.js
+++ polyglot2/polyglot/src/App.js
@@ -1,3 +1,9 @@
+import { 
+  BrowserRouter as Router, Routes, 
+  Route 
+} from "react-router-dom"; 
+import About from "./pages/Heft.js";
+
 
 function App() {
   return (
@@ -5,6 +11,16 @@ function App() {
       <head>
         <meta name="viewport" content="initial-scale=1, width=device-width" />
       </head>
+
+      <Router> 
+        <MyButton to="" /> 
+        <MyButton to="Heft" /> 
+        <Routes> 
+            <Route path="/" element={<Home />} /> 
+            <Route path="/profile"
+                element={<Heft />} /> 
+        </Routes> 
+      </Router> 
       
     </div>
   );
