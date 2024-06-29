43c00bf70f95c062e370d5af3a9b52b5adf59de8
Charles <131194505+Jurai8@users.noreply.github.com>
2024-06-29 21:13:57 +0000

    Vokabelheft table baby!!!

---
+++ Polyglot/polyglot-2/src/components/Table.js
@@ -0,0 +1,50 @@
+import * as React from 'react';
+import Table from '@mui/material/Table';
+import TableBody from '@mui/material/TableBody';
+import TableCell from '@mui/material/TableCell';
+import TableContainer from '@mui/material/TableContainer';
+import TableHead from '@mui/material/TableHead';
+import TableRow from '@mui/material/TableRow';
+import Paper from '@mui/material/Paper';
+
+function createData(word, translation) {
+  return {word, translation};
+}
+
+const rows = [
+  createData('Red', 'Rot'),
+  createData('Blue', 'Blau'),
+  createData('Yellow', 'Gelb'),
+  createData('The man', 'Der Mann')
+];
+
+export default function VocabBook() {
+  return (
+    <TableContainer component={Paper}>
+      <Table sx={{ minWidth: 650 }} aria-label="simple table">
+        <TableHead>
+          <TableRow>
+            {/* replace with variables in future */}
+            <TableCell>English</TableCell>
+            <TableCell align="right">German</TableCell>
+          </TableRow>
+        </TableHead>
+        <TableBody>  
+          {rows.map((row) => (
+            // generate table row component for each row
+            <TableRow
+              key={row.word} // identify each row by row.name (each name must be unique)
+              sx={{ '&:last-child td, &:last-child th': { border: 0 } }}
+            >
+              {/*the first value in the row */}
+              <TableCell component="th" scope="row">
+                {row.word}
+              </TableCell>
+              <TableCell align="right">{row.translation}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
+      </Table>
+    </TableContainer>
+  );
+}
--- Polyglot/polyglot-2/src/pages/Heft.js
+++ Polyglot/polyglot-2/src/pages/Heft.js
@@ -1,12 +1,14 @@
 import * as React from 'react';
-import AddWord from '../components/Modal';
+import VocabBook from '../components/Table'
+
 
 // button leading to current page should be removed
 export default function Heft () {
+    // <AddWord> will pop up as a modal when the user wants to enter a word
     return (
         <div>
-            <AddWord />
-            <h1>Guacamole</h1>
+            {/* create a shadow around the box*/}
+            <VocabBook />
         </div>
     )
 }
