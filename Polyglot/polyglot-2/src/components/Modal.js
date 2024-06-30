646ef53dfc814aa14239f54045aea37c14e79674
Charles <131194505+Jurai8@users.noreply.github.com>
2024-06-30 19:30:28 +0000

    idk

--- Polyglot/polyglot-2/src/components/Modal.js
+++ Polyglot/polyglot-2/src/components/Modal.js
@@ -3,10 +3,15 @@ import * as React from 'react';
 import Box from '@mui/material/Box';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
+import { useState } from 'react';
 
 
 export default function AddWord ({ onClose }) {
-    // when confirm is clicked close modal
+    const [input, setInput] = useState({
+        native: '',
+        translation: ''
+    })
+
     return (
         <div className='overlay'>
             <Box 
@@ -19,11 +24,11 @@ export default function AddWord ({ onClose }) {
                 autoComplete="off"
                 >
 
-                <TextField id="outlined-basic-english" label="English" variant="outlined" />
-                <TextField id="outlined-basic-german" label="German" variant="outlined" />
+                <TextField id="outlined-basic-english" label="English" name="native" variant="outlined" />
+                <TextField id="outlined-basic-german" label="German" name="translation" variant="outlined" />
     
                 <div id='confirm-word'>
-                    <Button variant="contained" onClick={onClose}>
+                    <Button variant="contained" onClick={onClose} >
                         Confirm
                     </Button>
                 </div>
