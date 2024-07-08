5a81e0984abbd8c94998ba2de429d8c81d1d7769
Charles <131194505+Jurai8@users.noreply.github.com>
2024-07-08 21:31:31 +0200

    integrated firebase

---
+++ Polyglot/polyglot-2/src/firebase.js
@@ -0,0 +1,19 @@
+import { initializeApp } from "firebase/app";
+import { getAnalytics } from "firebase/analytics";
+import { getFirestore } from "@firebase/firestore";
+
+const firebaseConfig = {
+  apiKey: "AIzaSyA7yhna5--zjgQHFuLTVvps4ZR82zoh_pM",
+  authDomain: "charlies-project-2fcac.firebaseapp.com",
+  databaseURL: "https://charlies-project-2fcac.firebaseio.com",
+  projectId: "charlies-project-2fcac",
+  storageBucket: "charlies-project-2fcac.appspot.com",
+  messagingSenderId: "871811973657",
+  appId: "1:871811973657:web:01d43adeb98d609826e088",
+  measurementId: "G-WXSBNZDZG6"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+const analytics = getAnalytics(app);
+const firestore =  getFirestore(app)
