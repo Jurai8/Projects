343f76d91fce213120c3046b833ddb15bfb2e391
Charles <131194505+Jurai8@users.noreply.github.com>
2024-07-12 23:54:06 +0200

    created asyn func, "compare"

--- Polyglot/polyglot-2/src/pages/Test.js
+++ Polyglot/polyglot-2/src/pages/Test.js
@@ -23,25 +23,40 @@ export default function Test() {
     const nextWord = () => {
         if (counter.current < vocabList.length - 1) {
             counter.current++;
-            setTestWord(vocabList[counter.current].translation);
+            setTestWord(vocabList[counter.current].word);
         } else {
             alert("No more words");
         }
     }
 
+    /* this function needs to be passed to heft
+    create a button to begin the test*/
     const word = async () => {
         try {
             const vocab = await getDocs(collection(firestore, "Vocabulary"));
 
             // Using map to transform each document's data and return an array of results
             vocabList.push(...vocab.docs.map(doc => doc.data()));
-            setTestWord(vocabList[0].translation); 
+            setTestWord(vocabList[0].word); 
             alert("begin");
         } catch (error) {
             console.log('Error fetching words from db:', error);
             throw error; // Propagate the error for handling elsewhere
         }
     }; 
+
+    const compare = async (answer) => {
+        // Simulate a fetch call
+        return new Promise((resolve, reject) => {
+            // user needs to write the translation
+            if (answer === vocabList[counter.current].translation) {
+                resolve("correct answer");
+            } else {
+                reject(new Error('Incorrect answer'));
+            }
+        }
+
+    )};
 
     return (
         <div>
@@ -56,13 +71,14 @@ export default function Test() {
                 >
                 <TextField id="standard-basic" label="Standard" variant="standard" type='text' inputRef={answer} />
                 {/*display block + justify content right */}
-                <Button variant="contained" onClick={() => {
-                    if (answer.current.value === word) {
+                <Button variant="contained" onClick={async () => {
+                    try {
+                      let result = await compare(answer.current.value);
+                       nextWord();
+                       alert(result);
+                    } catch (error) {
                         nextWord();
-                        return alert("correct");
-                    } else {
-                        nextWord();
-                        return alert("incorrect");
+                        alert(error.message);
                     }
                 }}>
                     Confirm
