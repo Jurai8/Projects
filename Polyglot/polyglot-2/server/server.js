3a868224ef9e8d649e2f60a5e2cccedea2d51500
Charles <131194505+Jurai8@users.noreply.github.com>
2024-07-14 19:35:09 +0200

    create server.js

---
+++ Polyglot/polyglot-2/server/server.js
@@ -0,0 +1,8 @@
+var admin = require("firebase-admin");
+
+var serviceAccount = require("./charlies-project-2fcac-firebase-adminsdk-hggn1-b0c290d5fb.json");
+
+admin.initializeApp({
+  databaseURL: "https://charlies-project-2fcac.firebaseio.com"
+});
+
