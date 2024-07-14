189220b05464ca6ac7005a9aa3877a9f97901662
Charles <131194505+Jurai8@users.noreply.github.com>
2024-07-14 22:09:48 +0200

    create user

--- Polyglot/polyglot-2/server/server.js
+++ Polyglot/polyglot-2/server/server.js
@@ -1,3 +1,5 @@
+
+// ignore for now
 var admin = require("firebase-admin");
 
 var serviceAccount = require("./charlies-project-2fcac-firebase-adminsdk-hggn1-b0c290d5fb.json");
@@ -6,3 +8,26 @@ admin.initializeApp({
   databaseURL: "https://charlies-project-2fcac.firebaseio.com"
 });
 
+
+
+function createUser() {
+    admin.auth()
+      .createUser({
+        email: '',
+        emailVerified: false,
+        password: '',
+        userName: '',
+        photoURL: 'http://www.example.com/12345678/photo.png',
+        disabled: false,
+      })
+      .then((userRecord) => {
+        // See the UserRecord reference doc for the contents of userRecord.
+        console.log('Successfully created new user:', userRecord.uid);
+      })
+      .catch((error) => {
+        console.log('Error creating new user:', error);
+      });
+  }
+  
+  // Call the createUser function
+  createUser();
--- Polyglot/polyglot-2/src/firebase.js
+++ Polyglot/polyglot-2/src/firebase.js
@@ -1,6 +1,7 @@
 import { initializeApp } from "firebase/app";
 import { getAnalytics } from "firebase/analytics";
 import { getFirestore } from "@firebase/firestore";
+import { getAuth } from "firebase/auth";
 
 const firebaseConfig = {
   apiKey: "AIzaSyA7yhna5--zjgQHFuLTVvps4ZR82zoh_pM",
@@ -16,4 +17,5 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const analytics = getAnalytics(app);
+const auth = getAuth(app);
 export const firestore =  getFirestore(app);
