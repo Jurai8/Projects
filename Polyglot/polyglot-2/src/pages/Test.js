92652cc21f28a60494fb65cd25d948dfaf282c2a
Charles <131194505+Jurai8@users.noreply.github.com>
2024-07-23 21:36:57 +0200

    Test initializes on Begin

--- Polyglot/polyglot-2/src/pages/Test.js
+++ Polyglot/polyglot-2/src/pages/Test.js
@@ -18,22 +18,27 @@ export default function Test() {
 
 
     const [word, setWord] = useState('');
-    const [count, setCount] = useState(0);
+    const [count, setCount] = useState(null);
+    const [evaluation, setEvaluation] = useState('')
     const vocabListRef = useRef([]);
+    const answer = useRef(null);
 
     // this needs to happen before the mount
+
     useEffect(() => {
         setWord(vocabListRef.current[count])
+        console.log("hello");
     }, [count])
 
     const initializeVocab = async () => {
         const newWords = await FetchVocab();
         vocabListRef.current = newWords;
         console.log(vocabListRef.current);
-        setCount(0); // Reset count to 0 to start from the first word
-      };
+        // Reset count to 0 to start from the first word
+        setCount(0)
+    };
 
-      // checkAnswer
+    
 
     return (
         <div>
@@ -46,13 +51,15 @@ export default function Test() {
                 noValidate
                 autoComplete="off"
                 >
-                <TextField id="standard-basic" label="Standard" variant="standard" type='text'/> {/*inputRef={answer} */ }
+                <TextField id="standard-basic" label="Standard" variant="standard" type='text'/> inputRef={answer}
                 <Button variant="contained" 
                 onClick={() => setCount((prevCount) => prevCount + 1)}>
                     Confirm
                 </Button>
 
-                <Button variant="contained" onClick={initializeVocab}>
+                <Button variant="contained" onClick={() => {
+                     initializeVocab()  
+                }}>
                     Begin
                 </Button>
             </Box>
