80833cef6c17ce11662139250dd58d8e9ef5ebd4
Charles <131194505+Jurai8@users.noreply.github.com>
2025-02-16 18:03:01 +0200

    i

--- Polyglot/polyglot-2/src/hooks/useTest.js
+++ Polyglot/polyglot-2/src/hooks/useTest.js
@@ -14,27 +14,5 @@ export default async function useTest(user) {
 
     // Match word to translation and vice versa
         // fetchvocab()
-        async getVocab(listName) {
-            if (this.user) {
-                const userId = this.user.uid;
-                // get vocab 
-                try {
-                    const getVocabdocs = await getDocs(collection(firestore, "Users", userId, listName)); 
-    
-                    getVocabdocs.forEach((doc) => {
-                        this.vocab.push({
-                            native: doc.data().word,
-                            translation: doc.data().translation
-                        });
-                    });
-                } catch (error) {
-                    console.error("could not get vocab for test");
-                }
-                
-                // how to return a randomized order of the array ?
-                return this.vocab;
-            } else {
-                console.error("User not logged in");
-            }
-        }
+       // getVocab
 }
