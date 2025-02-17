5166e29a22e5b45e0400e846adc783876ca2a0b6
Charles <131194505+Jurai8@users.noreply.github.com>
2025-02-17 06:56:07 +0200

    figuring out test

--- Polyglot/polyglot-2/src/hooks/useTest.js
+++ Polyglot/polyglot-2/src/hooks/useTest.js
@@ -1,12 +1,45 @@
+import { use } from 'react';
+import { useState, useEffect} from 'react';
+import { useFetchVocab } from 'useSetVocab.js'
+
 
 
 // hook to initialize different tests
+export default async function useTest(user, listname) {
+    const { getVocab } = useFetchVocab(user);
 
-
-export default async function useTest(user) {
     // first decide which test the user will write
+    const [testType, setTestType] = useState('');
 
-    // track score (usestate)
+    // state to iterate through array
+    const [vocab, setVocab] = useState([]);
+
+    // state to track score
+    const [score, setScore] =  useState();
+
+    // begin test using useEffect
+    const [isTesting, setIsTesting] = useState(false);
+
+    useEffect(() => {
+        if (isTesting) {
+            const sourceTransTest = () => {
+
+            }
+        }
+    }, [isTesting, testType])
+
+    
+    useEffect(() => {
+        const getvocablist = async () => {
+           const vocablist = await getVocab(listname);
+
+           setVocab(vocablist);
+        }
+
+        getvocablist();
+        
+    }, [getVocab, listname])
+
 
     // Match word to pos
 
